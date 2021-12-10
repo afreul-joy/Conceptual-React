0a7e813a5ead662f8da00ee81f08411ad3c0ceed
AFreul kaium Joy <afreuljoy@gmail.com>
2021-12-11 00:02:00 +0600

    first commit

---
+++ src/Component/Device/Device.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Device = () => {
+    const [steps, setSteps] = useState(0);
+
+    return (
+        <div>
+            <h2>My Steps {steps} </h2>
+        </div>
+    );
+};
+
+export default Device;
