fd855bf9508e75c41be9fbdc9eadf362b4548bab
AFreul kaium Joy <afreuljoy@gmail.com>
2021-12-11 01:04:42 +0600

    test

--- src/Component/Device/Device.js
+++ src/Component/Device/Device.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const Device = () => {
     const [steps, setSteps] = useState(0);
-
+    
     return (
         <div>
             <h2>My Steps {steps} </h2>
